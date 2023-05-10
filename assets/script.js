bd72bc18de5c1577e8ea54b4ae82219c8866d47c
hasherlaws18 <houstonasherlaws@gmail.com>
2023-05-09 22:24:20 -0500

    some new changes to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questioncontainer = document.getElementById('question-container')
+const questionText = document.getElementById('question');
+const Scoredisplay = document.getElementById('Score')
 const timerdisplay = document.getElementById('timerdisplay');
 const answerbuttons = document.querySelectorAll('.button.is-black')
 const scoreConatainer = document.querySelector('.Score-Container');
+const answerText = document.getElementById('choice-id');
 const Answer1 = document.getElementById('Answer-1')
 const Answer2 = document.getElementById('Answer-2')
 const Answer3 = document.getElementById('Answer-3')
@@ -12,9 +15,10 @@ var timeLeft =60;
 var TimeInterval;
 var currentQuestionIndex;
 var score;
+let i = 0;
 
 
-  const questions = [
+  const Quizquestions = [
     {
         question: 'What is Used to style a page',
         Choices : [
@@ -70,21 +74,31 @@ var score;
 
   startButton.addEventListener('click', startQuiz);
   nextButton.addEventListener('click', () => {
-    currentQuestionIndex;
+    currentQuestionIndex++;
     setNextQuestion()
   });
 
-function startQuiz() {
-    currentQuestionIndex = 0;
+  function startQuiz() {
     score = 0;
-    timeLeft = 60;
-    scoreConatainer.innerHTML = '';
+    currentQuestionIndex = 0;
     startButton.classList.add('hide');
     questioncontainer.classList.remove('hide');
     setNextQuestion();
-    TimeInterval = setInterval(countdown, 1000);
-}
+  }
 
-function setNextQuestion() {
-    
-}
+  function setNextQuestion() {
+    showQuestion(Quizquestions[currentQuestionIndex]);
+  }
+
+  function showQuestion(question) {
+     questionText.innerText = question.question;
+     Answer1.innerText = question.Choices[0].text;
+     Answer1.dataset.id = question.Choices[0].id;
+     Answer2.innerText = question.Choices[1].text;
+     Answer2.dataset.id = question.Choices[1].id;
+     Answer3.innerText = question.Choices[2].text;
+     Answer3.dataset.id = question.Choices[2].id;
+     Answer4.innerText = question.Choices[3].text;
+     Answer4.dataset.id = question.Choices[3].id;
+  }
+  
