9c77d035b1ab3c13602f5229247ea950a806cf51
hasherlaws18 <houstonasherlaws@gmail.com>
2023-05-09 23:41:05 -0500

    added to script and tryed to fix some bugs

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
-const questioncontainer = document.getElementById('question-container')
-const questionText = document.getElementById('question');
+const Questioncontainer = document.getElementById('Questions-container')
+const questionText = document.getElementById('Question');
 const Scoredisplay = document.getElementById('Score')
 const timerdisplay = document.getElementById('timerdisplay');
-const answerbuttons = document.querySelectorAll('.button.is-black')
 const scoreConatainer = document.querySelector('.Score-Container');
 const answerText = document.getElementById('choice-id');
 const Answer1 = document.getElementById('Answer-1')
@@ -76,29 +75,61 @@ let i = 0;
   nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
     setNextQuestion()
+    
   });
 
   function startQuiz() {
     score = 0;
     currentQuestionIndex = 0;
     startButton.classList.add('hide');
-    questioncontainer.classList.remove('hide');
+    Questioncontainer.classList.remove('hide');
     setNextQuestion();
+    timer();
   }
 
   function setNextQuestion() {
     showQuestion(Quizquestions[currentQuestionIndex]);
   }
 
-  function showQuestion(question) {
-     questionText.innerText = question.question;
-     Answer1.innerText = question.Choices[0].text;
-     Answer1.dataset.id = question.Choices[0].id;
-     Answer2.innerText = question.Choices[1].text;
-     Answer2.dataset.id = question.Choices[1].id;
-     Answer3.innerText = question.Choices[2].text;
-     Answer3.dataset.id = question.Choices[2].id;
-     Answer4.innerText = question.Choices[3].text;
-     Answer4.dataset.id = question.Choices[3].id;
+  function showQuestion(Question) {
+     questionText.innerText = Question.question;
+     Answer1.innerText = Question.Choices[0].text;
+     Answer1.dataset.id = Question.Choices[0].id;
+     Answer2.innerText = Question.Choices[1].text;
+     Answer2.dataset.id = Question.Choices[1].id;
+     Answer3.innerText = Question.Choices[2].text;
+     Answer3.dataset.id = Question.Choices[2].id;
+     Answer4.innerText = Question.Choices[3].text;
+     Answer4.dataset.id = Question.Choices[3].id;
+  }
+
+  function timer(){
+    timerdisplay.innerText = timeLeft;
+    TimeInterval = setInterval(() => {
+      if (timeLeft <= 0) {
+        clearInterval(TimeInterval);
+        endquiz();
+      }
+      timerdisplay.innerText = timeLeft;
+      timeLeft -= 1;
+    }, 1000);
+  }
+
+  function ScoreCounter(selectedAnswerId) {
+    const currentQuestion = Quizquestions[currentQuestionIndex];
+    if (selectedAnswerId === currentQuestion.CorrectAnswer) {
+      score += 1;
+    } else{
+      score -=1;
+    }
+    Scoredisplay.innerText = `Score: ${score}`;
+  }
+  
+  
+
+  function endquiz() {
+    clearInterval(TimeInterval);
+    scoreConatainer.classList.remove('hide');
+    Scoredisplay.innerText = `Your Final Score: ${score}`;
   }
   
